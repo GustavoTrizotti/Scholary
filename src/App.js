966f127c1221114eac8db32558ed720af218497b
Gustavo Trizotti <gustavotrizotti14@gmail.com>
2023-05-26 20:44:03 -0300

    feat: register/remove student implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterStudent from './RegisterStudent';
+import StudentDetails from './StudentDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/register' element={<RegisterStudent />}/>
+            <Route path='/students/:id' element={<StudentDetails />}/>
+            <Route path='*' element={<NotFound />}/>
           </Routes>
         </div>
       </div>
--- src/StudentList.js
+++ src/StudentList.js
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 const StudentList = ({ students, title }) => {
     return ( 
         <div className="student-list">
             <h2>{title}</h2>
             { students.map((student) => (
-                <div className="student-preview" key={student.id}>
-                    <h2>Student: {student.name}</h2>
-                    <p>E-mail: {student.email}</p>
-                </div>
+                <Link to={`/students/${student.id}`} key={student.id}>
+                    <div className="student-preview" key={student.id}>
+                        <h2><b>Student:</b> {student.name}</h2>
+                        <p><b>E-mail:</b> {student.email}</p>
+                        <p><b>Overall Grade:</b> { student.grades.reduce((a, b) => a + b) / student.grades.length }</p>
+                    </div>
+                </Link>
+                
             )) }
         </div>
     );
